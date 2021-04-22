9ef3907e2e276c8f1a524d668171eb5b539fafd8
Israt Jahan Jui <jahanjuiisrat@gmail.com>
2021-04-23 02:20:15 +0600

    input field added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Body = () => {
-    return (
-        <div>
+
+    const [value, setValue] = useState({
+        name: "",
+        details: "",
+        
+    })
+    const [flag,setFlag]= useState(false)
+
+    const handleChange = (e) => {
+        setValue({
+            ...value, // age:"27",name:"JUI" next task to do list,
+            [e.target.name]: e.target.value
             
-        </div>
+        })
+        setFlag(false);
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(value);
+        setFlag(true);
+        
+    }
+    return (
+        <>
+        <form onSubmit={handleClick}>
+            <input placeholder="Task Name" value={value.name} onChange={handleChange} name="name" />
+            <input placeholder="Details" value={value.age} onChange={handleChange} name="details" />
+            <button>Submit</button>
+        </form>
+{flag ? <div>
+         {value.name} 
+         {value.details}  
+         
+        </div>: null}
+        
+        </>
+    
+
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import Body from '../components/Body';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
