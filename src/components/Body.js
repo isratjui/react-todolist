3ff9c1cdcc0eee1c4af03aac5ff0cbc8fcc1ad5c
Israt Jahan Jui <jahanjuiisrat@gmail.com>
2021-04-23 23:26:21 +0600

    Todo list done

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,41 +4,72 @@ const Body = () => {
 
     const [value, setValue] = useState({
         name: "",
-        details: "",
-        
+
     })
-    const [flag,setFlag]= useState(false)
+   
+    const [flag, setFlag] = useState(false)
+    const [todoList, setTodoList] = useState([]);
+
+
 
     const handleChange = (e) => {
         setValue({
             ...value, // age:"27",name:"JUI" next task to do list,
             [e.target.name]: e.target.value
-            
+
         })
+        //setTodoList({
+        //  ...value, // age:"27",name:"JUI" next task to do list,
+        // [e.target.name]: e.target.value
+        //})
+
         setFlag(false);
     }
-
+   // const [task, setTask] = useState();
+    
     const handleClick = (e) => {
         e.preventDefault();
         console.log(value);
         setFlag(true);
-        
+       // setTask(value.name);
+        setTodoList((todoList) => [
+            value.name,...todoList,
+            
+          ]); 
+          console.log(todoList);
+
     }
+   
+    
+    //  setTodoList(value);
     return (
         <>
-        <form onSubmit={handleClick}>
-            <input placeholder="Task Name" value={value.name} onChange={handleChange} name="name" />
-            <input placeholder="Details" value={value.age} onChange={handleChange} name="details" />
-            <button>Submit</button>
-        </form>
-{flag ? <div>
-         {value.name} 
-         {value.details}  
-         
-        </div>: null}
-        
+            <form onSubmit={handleClick}>
+                <input placeholder="Task Name" value={value.name} onChange={handleChange} name="name" />
+                
+                <button>Submit</button>
+            </form>
+            {flag ? <div>
+             {value.name}
+                
+
+            </div> : null}
+            
+            <h2>Task List</h2>
+            <ul>
+                {
+                    todoList.map((item, i) => {
+                       // if (i !== 0) {
+                            return (
+                                <li key={i}>{item} {i}</li>
+                            )
+                       // }
+                    })
+                }
+            </ul>
+
         </>
-    
+
 
     );
 };
